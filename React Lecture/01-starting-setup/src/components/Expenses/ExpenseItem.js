86cc9eaa70894d75107a5c797f9578b5e1e83abf
Taiye Bandipo <taiye.bandipo@teamapt.com>
2022-08-09 17:23:51 +0100

    added new components

--- React Lecture/01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ React Lecture/01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 function ExpenseItem({ title, amount, date }) {
+  const [expenseTitle, setExpenseTitle] = useState(title);
+
+  const clickHandler = () => {
+    setExpenseTitle("clicked");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{expenseTitle}</h2>
         <div className="expense-item__price">{amount}</div>
       </div>
+      <button onClick={clickHandler}>change title</button>
     </Card>
   );
 }

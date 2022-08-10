0253f31d4b16ff8daa4adf1ba1ae841493e4499e
Taiye Bandipo <taiye.bandipo@teamapt.com>
2022-08-11 00:50:46 +0100

    implement Two Way Binding

--- React Lecture/01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ React Lecture/01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -68,7 +68,11 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            onChange={titleChangeHandler}
+            value={enteredTitle}
+          />
         </div>
 
         <div className="new-expense__control">
@@ -78,6 +82,7 @@ const ExpenseForm = () => {
             min="0.01"
             step="0.01"
             onChange={amountChangeHandler}
+            value={enteredAmount}
           />
         </div>
 
@@ -88,6 +93,7 @@ const ExpenseForm = () => {
             min="2019-01-01"
             max="2022-12-31"
             onChange={dateChangeHandler}
+            value={enteredDate}
           />
         </div>
       </div>
